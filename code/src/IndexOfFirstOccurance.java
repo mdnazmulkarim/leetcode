55341c6ab3f6c11cb9971eeb33c5d1bd995a7fa1
mdnazmulkarim <nazmul.karim@smilecdr.com>
2023-01-22 14:18:33 -0700

    IndexOfFirstOccurance

---
+++ code/src/IndexOfFirstOccurance.java
@@ -0,0 +1,24 @@
+public class IndexOfFirstOccurance {
+    public static void main(String[] args) {
+        System.out.println(new IndexOfFirstOccurance().strStr("mississippi", "issip"));
+       // System.out.println(new IndexOfFirstOccurance().strStr("sadstorysad", "sad"));
+       // System.out.println(new IndexOfFirstOccurance().strStr("leetcode", "leeto"));
+    }
+
+    public int strStr(String haystack, String needle) {
+        int i=0;
+        while(i<haystack.length()) {
+            int j=i;
+            int k = 0;
+            while(j < haystack.length() && haystack.charAt(j) == needle.charAt(k) && k < needle.length()) {
+                j++;
+                k++;
+                if(k == needle.length()){
+                    return i;
+                }
+            }
+            i++;
+        }
+        return -1;
+    }
+}
