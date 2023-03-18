e9aebdb716843a5b26d1e0a5e066fdeb41956076
mdnazmulkarim <mdnazmulkarim@u.boisestate.edu>
2023-03-18 10:58:50 -0600

    Power

---
+++ code/src/Power.java
@@ -0,0 +1,26 @@
+public class Power {
+    public static void main(String[] args) {
+        System.out.println(new Power().myPow(2,-2));
+    }
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return fastPow(x, N);
+    }
+
+    private double fastPow(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+}
