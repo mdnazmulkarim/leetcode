b4405fe28d27bde2f0fcb355d909fd1c1e4ea892
mdnazmulkarim <mdnazmulkarim@u.boisestate.edu>
2024-02-01 19:18:11 -0700

    add sort alphabetically

--- code/src/GroupAnagram.java
+++ code/src/GroupAnagram.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class GroupAnagram {
     public static void main(String[] args) {
@@ -9,23 +10,55 @@ public class GroupAnagram {
 
         int[] arr = new int[26];
         Arrays.fill(arr, 0);
-        Map<String, List<String>> map = new HashMap<>();
+        Map<String, List<Anagram>> map = new HashMap<>();
 
         for(String str : strs) {
             for(char c : str.toCharArray()) {
                 int ind = c - 'a';
                 arr[ind] = arr[ind] + 1;
             }
-            StringBuilder sb = new StringBuilder("");
+            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < 26; i++) {
                 if(arr[i] != 0) {
                     sb.append((char)(i + 97)).append("#").append(arr[i]);
                     arr[i] = 0;
                 }
             }
-            map.computeIfAbsent(sb.toString(), key -> new ArrayList<String>()).add(str);
+            map.computeIfAbsent(sb.toString(), key -> new ArrayList<>()).add(new Anagram(str));
         }
 
-        return map.values().stream().toList();
+        for(List<Anagram> list : map.values()) {
+            Collections.sort(list);
+        }
+
+        List<List<String>> newList = new ArrayList<>();
+        for(List<Anagram> list : map.values()) {
+            newList.add(list.stream().map(item -> item.str).collect(Collectors.toList()));
+        }
+
+        return newList;
+    }
+
+    static class Anagram implements Comparable<Anagram>{
+
+        public Anagram(String str) {
+            this.str = str;
+            calculate();
+        }
+        int val;
+        String str;
+
+        void calculate() {
+            int multiplier = 1;
+            for(int  i= str.length(); i > 0; i--) {
+                val = val + (str.charAt(i -1) - 'a') * multiplier;
+                multiplier *= 10;
+            }
+        }
+
+        @Override
+        public int compareTo(Anagram o) {
+            return Integer.compare(this.val, o.val);
+        }
     }
 }
